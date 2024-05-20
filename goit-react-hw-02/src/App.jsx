3c4d74613ef3876c9bc92a8ed85c3efcf3677a7b
Olga Kryvoshlyk <okryvoshlyk@gmail.com>
2024-05-20 11:09:40 -0700

    task 6 completed

--- goit-react-hw-02/src/App.jsx
+++ goit-react-hw-02/src/App.jsx
@@ -4,25 +4,34 @@ import Options from "./components/Options/Options";
 import Feedback from "./components/Feedback/Feedback";
 import Notification from "./components/Notification/Notification";
 
-const App = () => {
-    const [feedbackValue, setFeedbackValue] = useState(() => {
-    const savedFeedback = localStorage.getItem('savedFeedbacks');
-    if (savedFeedback) {
-      return JSON.parse(savedFeedback);
-    } else {
-    return {
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    };
-    }
-    });
+const initialFeedbackValue = {
+        good: 0,
+        neutral: 0,
+        bad: 0,
+}
     
-useEffect(() => {
-    localStorage.setItem("'savedFeedbacks'", JSON.stringify(feedbackValue));
-  }, [feedbackValue]);
+const FEEDBACK_LOCALSTORAGE_KEY = 'savedFeedbacks';
 
-    const updateFeedback = feedbackValue => {
+const App = () => {
+    const [feedbackValue, setFeedbackValue] = useState(initialFeedbackValue);
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        const savedFeedbacks = localStorage.getItem(FEEDBACK_LOCALSTORAGE_KEY);
+        setFeedbackValue(JSON.parse(savedFeedbacks));
+    }, []);
+    
+    
+    useEffect(() => {
+        if (mounted) {
+            localStorage.setItem(FEEDBACK_LOCALSTORAGE_KEY, JSON.stringify(feedbackValue));
+        } else {
+            setMounted(true);
+        }
+    }, [feedbackValue, mounted]);
+    
+
+const updateFeedback = feedbackValue => {
         setFeedbackValue((prev) => {
             return {
                 ...prev,
@@ -37,12 +46,13 @@ useEffect(() => {
       neutral: 0,
       bad: 0,
     });
+     console.log(12);
  };
     
 const totalFeedback =
     feedbackValue.good + feedbackValue.neutral + feedbackValue.bad;
   
-    const positiveFeedback =
+const positiveFeedback =
         totalFeedback > 0 ? Math.round(((feedbackValue.good+feedbackValue.neutral) / totalFeedback) * 100) : 0;
 
     return(
