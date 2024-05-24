55da380e5545a5bb3565603b9ecd3680296c3a47
Olga Kryvoshlyk <okryvoshlyk@gmail.com>
2024-05-24 11:04:40 -0700

    Fixed mainContentStyles, changed feedback initial value

--- src/App.jsx
+++ src/App.jsx
@@ -3,73 +3,69 @@ import FeedbackDescription from "./components/Description/Description";
 import Options from "./components/Options/Options";
 import Feedback from "./components/Feedback/Feedback";
 import Notification from "./components/Notification/Notification";
+import css from "./App.module.css";
 
 const initialFeedbackValue = {
-        good: 0,
-        neutral: 0,
-        bad: 0,
-}
-    
-const FEEDBACK_LOCALSTORAGE_KEY = 'savedFeedbacks';
+  good: 0,
+  neutral: 0,
+  bad: 0,
+};
+
+const FEEDBACK_LOCALSTORAGE_KEY = "savedFeedbacks";
 
 const App = () => {
-    const [feedbackValue, setFeedbackValue] = useState(initialFeedbackValue);
-    const [mounted, setMounted] = useState(false);
+  const [feedbackValue, setFeedbackValue] = useState(() => {
+    const savedFeedback = localStorage.getItem(FEEDBACK_LOCALSTORAGE_KEY);
+    return savedFeedback ? JSON.parse(savedFeedback) : initialFeedbackValue;
+  });
 
-    useEffect(() => {
-        const savedFeedbacks = localStorage.getItem(FEEDBACK_LOCALSTORAGE_KEY);
-        const startValue = savedFeedbacks ? JSON.parse(savedFeedbacks) :  initialFeedbackValue;
-        setFeedbackValue(startValue);
-    }, []);
-    
-    
-    useEffect(() => {
-        if (mounted) {
-            localStorage.setItem(FEEDBACK_LOCALSTORAGE_KEY, JSON.stringify(feedbackValue));
-        } else {
-            setMounted(true);
-        }
-    }, [feedbackValue, mounted]);
-    
+  useEffect(() => {
+    localStorage.setItem(
+      FEEDBACK_LOCALSTORAGE_KEY,
+      JSON.stringify(feedbackValue)
+    );
+  }, [feedbackValue]);
 
-const updateFeedback = feedbackValue => {
-        setFeedbackValue((prev) => {
-            return {
-                ...prev,
-                [feedbackValue]: prev[feedbackValue] + 1
-            }
-        })
-    }
-    
- const resetFeedback = () => {
-    setFeedbackValue({
-      good: 0,
-      neutral: 0,
-      bad: 0,
+  const updateFeedback = (feedbackValue) => {
+    setFeedbackValue((prev) => {
+      return {
+        ...prev,
+        [feedbackValue]: prev[feedbackValue] + 1,
+      };
     });
-     console.log(12);
- };
-    
-const totalFeedback =
+  };
+
+  const resetFeedback = () => {
+    setFeedbackValue(initialFeedbackValue);
+  };
+
+  const totalFeedback =
     feedbackValue.good + feedbackValue.neutral + feedbackValue.bad;
-  
-const positiveFeedback =
-        totalFeedback > 0 ? Math.round(((feedbackValue.good+feedbackValue.neutral) / totalFeedback) * 100) : 0;
 
-    return(
-        <>
-            <FeedbackDescription />
-            <Options updateFeedback={updateFeedback}
-                totalFeedback={totalFeedback}
-                resetFeedback={resetFeedback}
-            />
-            {totalFeedback ?
-                <Feedback feedbackData={feedbackValue}
-                totalFeedback={totalFeedback}
-                positiveFeedback={positiveFeedback}/> :
-                <Notification />}
-        </>
-    )
-}
+  const positiveFeedback =
+    totalFeedback > 0
+      ? Math.round((feedbackValue.good / totalFeedback) * 100)
+      : 0;
 
-export default App; 
+  return (
+    <div className={css.mainContent}>
+      <FeedbackDescription />
+      <Options
+        updateFeedback={updateFeedback}
+        totalFeedback={totalFeedback}
+        resetFeedback={resetFeedback}
+      />
+      {totalFeedback ? (
+        <Feedback
+          feedbackData={feedbackValue}
+          totalFeedback={totalFeedback}
+          positiveFeedback={positiveFeedback}
+        />
+      ) : (
+        <Notification />
+      )}
+    </div>
+  );
+};
+
+export default App;
